a352f0530e5ae3143382d0ef1c94dbf7ee9770ed
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-09-05 00:36:22 -0300

    fix: solve authorization bug

--- src/main/java/com/tzuchaedahy/compass_ecommerce_challenge/CompassEcommerceChallengeApplication.java
+++ src/main/java/com/tzuchaedahy/compass_ecommerce_challenge/CompassEcommerceChallengeApplication.java
@@ -2,8 +2,10 @@ package com.tzuchaedahy.compass_ecommerce_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class CompassEcommerceChallengeApplication {
 
 	public static void main(String[] args) {
